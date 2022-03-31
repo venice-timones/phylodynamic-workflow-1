75b70f61acd608339a9ed69beb3d0191af785530
Imari Joy Borda <imari.borda2018@gmail.com>
2022-03-31 12:18:37 +0800

    Refactor

--- 07-computePriors/scripts/info.R
+++ 07-computePriors/scripts/info.R
@@ -1,60 +1,45 @@
 ### Load libraries
 library("dplyr")
 library("lubridate")
+library("stringr")
+
 
 ### Read data
-metadata <- read.delim(file = paste0("input/mindanao.tsv"), sep = '\t', header = TRUE)
-metadata$date <- as.Date(metadata$date, format="%Y-%m-%d")
+mindanao <- read.delim(file = paste0("input/mindanao.tsv"), sep = '\t', header = TRUE)
+mindanao$date <- as.Date(mindanao$date, format="%Y-%m-%d")
+
 bdmm <- read.delim(file = paste0("input/bdmm.tsv"), sep = '\t', header = TRUE)
 bdmm$date <- as.Date(bdmm$date, format="%Y-%m-%d")
 
-### Count number of samples per region
-count <- data.frame(table(metadata$division)) 
 
-### Obtain date of youngest and oldest sampes per region
-sampdate <- metadata %>% 
-  select(division, date) %>%
-  group_by(division) %>%
-  summarise(
-    oldest = min(date, na.rm = T),
-    youngest = max(date, na.rm = T)
-  ) %>%
-  arrange(division)
+### Define Regions
+locations <- c(str_sort(unique(mindanao$division)), "Mindanao", "BDMM")
+info <- data.frame()
 
-### Calculate date range and convert dates to decimals
-sampdate <- transform(sampdate, 
-                      range = youngest - oldest,
-                      oldestDecimal = decimal_date(oldest),
-                      youngestDecimal = decimal_date(youngest))
 
-### Aggregate all info into one data frame
-info <- data.frame(count[1], count[2], sampdate[2], sampdate[3], sampdate[4], sampdate[5], sampdate[6])
+### Write info
+for (location in locations) {
+  if (location == "Mindanao") {
+    subset <- mindanao
+  } else if (location == "BDMM") {
+    subset <- bdmm
+  } else {
+    subset <- mindanao[mindanao$division == location, ]
+  }
+  subsetInfo <- data.frame(location, 
+                      count(subset),
+                      min(subset$date), 
+                      max(subset$date), 
+                      max(subset$date)-min(subset$date),
+                      min(decimal_date(subset$date)),
+                      max(decimal_date(subset$date)))
+  info <- rbind(info, subsetInfo)
+}
 colnames(info) <- c("region","cleanSamples","oldestSample", "youngestSample", "dateRange", "oldestSampleDecimal", "youngestSampleDecimal")
 
-### Add info for Mindanao (overall)
-overall <- data.frame("Mindanao", 
-                      sum(info$cleanSamples), 
-                      min(info$oldestSample), 
-                      max(info$youngestSample), 
-                      max(info$youngestSample)-min(info$oldestSample),
-                      min(info$oldestSampleDecimal),
-                      max(info$youngestSampleDecimal))
-colnames(overall) <- colnames(info)
-
-### Add info for bdmm (mindanao, but subsampled)
-bdmm1 <- data.frame("BDMM", 
-                    count(bdmm),
-                    min(bdmm$date), 
-                    max(bdmm$date), 
-                    max(bdmm$date)-min(bdmm$date),
-                    min(decimal_date(bdmm$date)),
-                    max(decimal_date(bdmm$date)))
-colnames(bdmm1) <- colnames(info)
-
-info.final <- rbind(info, overall, bdmm1)
 
 ### Save files 
-write.csv(x = info.final,
+write.csv(x = info,
           file = "output/info.csv",
           row.names = FALSE)
 
