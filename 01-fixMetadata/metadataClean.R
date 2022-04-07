98593271845ce21c11894122e235f55887f9a129
Imari Joy Borda <imari.borda2018@gmail.com>
2022-04-07 10:51:57 +0800

    Refactor

--- 01-fixMetadata/metadataClean.R
+++ 01-fixMetadata/metadataClean.R
@@ -2,95 +2,63 @@
 library("data.table")
 
 ### Function to make region names consistent with long_lat file.
-rename <- function(metadata) {
-  # Change Autonomous Region In Muslim Mindanao to Bangsamoro Autonomous Region In Muslim Mindanao
-  metadata$division <- ifelse(metadata$division == "Autonomous Region In Muslim Mindanao", "Bangsamoro Autonomous Region In Muslim Mindanao", metadata$division) 
-  metadata$division_exposure <- ifelse(metadata$division_exposure == "Autonomous Region In Muslim Mindanao", "Bangsamoro Autonomous Region In Muslim Mindanao", metadata$division_exposure) 
+fixName <- function(metadata) {
+  
+  ### Function to rename region name
+  rename <- function(arr, correct) {
+    for (loc in arr) {
+      metadata[['division']] <- ifelse(metadata[['division']] == loc, correct, metadata[['division']]) 
+      metadata[['division_exposure']] <- ifelse(metadata[['division_exposure']] == loc, correct, metadata[['division_exposure']]) 
+    }
+    return (metadata)
+  }
+  
+  metadata <- rename(c("Autonomous Region In Muslim Mindanao", 
+                       "Bangsamoro Autonomous Region in Muslim Mindanao"), 
+                       "Bangsamoro Autonomous Region In Muslim Mindanao")
+  
+  metadata <- rename(c("Caloocan City", 
+                       "NCR",
+                       "Manila",
+                       "Manila City",
+                       "Navotas City",
+                       "Pasay City",
+                       "Quezon City",
+                       "Taguig City"), 
+                       "National Capital Region")
+  
+  metadata <- rename(c("Davao",
+                       "Region XI (Davao Region)"), 
+                       "Davao Region")
+  
+  metadata <- rename(c("Region III"), 
+                       "Central Luzon")
+  
+  metadata <- rename(c("Region IV-A"), 
+                       "Calabarzon")
 
-  # Change Bangsamoro Autonomous Region in Muslim Mindanao to Bangsamoro Autonomous Region In Muslim Mindanao
-  metadata$division <- ifelse(metadata$division == "Bangsamoro Autonomous Region in Muslim Mindanao", "Bangsamoro Autonomous Region In Muslim Mindanao", metadata$division) 
-  metadata$division_exposure <- ifelse(metadata$division_exposure == "Bangsamoro Autonomous Region in Muslim Mindanao", "Bangsamoro Autonomous Region In Muslim Mindanao", metadata$division_exposure) 
+  metadata <- rename(c("Region IV-B"), 
+                       "Mimaropa")
   
-  # Change Caloocan City to National Capital Region
-  metadata$division <- ifelse(metadata$division == "Caloocan City", "National Capital Region", metadata$division) 
-  metadata$division_exposure <- ifelse(metadata$division_exposure == "Caloocan City", "National Capital Region", metadata$division_exposure) 
-  
-  # Change Davao to Davao Region
-  metadata$division <- ifelse(metadata$division == "Davao", "Davao Region", metadata$division) 
-  metadata$division_exposure <- ifelse(metadata$division_exposure == "Davao", "Davao Region", metadata$division_exposure) 
-  
-  # Change Manila to National Capital Region
-  metadata$division <- ifelse(metadata$division == "Manila", "National Capital Region", metadata$division) 
-  metadata$division_exposure <- ifelse(metadata$division_exposure == "Manila", "National Capital Region", metadata$division_exposure) 
-  
-  # Change Manila City to National Capital Region
-  metadata$division <- ifelse(metadata$division == "Manila City", "National Capital Region", metadata$division) 
-  metadata$division_exposure <- ifelse(metadata$division_exposure == "Manila City", "National Capital Region", metadata$division_exposure) 
-  
-  # Change NCR to National Capital Region
-  metadata$division <- ifelse(metadata$division == "NCR", "National Capital Region", metadata$division) 
-  metadata$division_exposure <- ifelse(metadata$division_exposure == "NCR", "National Capital Region", metadata$division_exposure) 
-  
-  # Change Navotas City to National Capital Region
-  metadata$division <- ifelse(metadata$division == "Navotas City", "National Capital Region", metadata$division) 
-  metadata$division_exposure <- ifelse(metadata$division_exposure == "Navotas City", "National Capital Region", metadata$division_exposure) 
-  
-  # Change Pasay City to National Capital Region
-  metadata$division <- ifelse(metadata$division == "Pasay City", "National Capital Region", metadata$division) 
-  metadata$division_exposure <- ifelse(metadata$division_exposure == "Pasay City", "National Capital Region", metadata$division_exposure) 
-  
-  # Change Quezon City to National Capital Region
-  metadata$division <- ifelse(metadata$division == "Quezon City", "National Capital Region", metadata$division) 
-  metadata$division_exposure <- ifelse(metadata$division_exposure == "Quezon City", "National Capital Region", metadata$division_exposure) 
-  
-  # Change Region III to Central Luzon
-  metadata$division <- ifelse(metadata$division == "Region III", "Central Luzon", metadata$division) 
-  metadata$division_exposure <- ifelse(metadata$division_exposure == "Region III", "Central Luzon", metadata$division_exposure) 
-  
-  # Change Region IV-A to Calabarzon
-  metadata$division <- ifelse(metadata$division == "Region IV-A", "Calabarzon", metadata$division) 
-  metadata$division_exposure <- ifelse(metadata$division_exposure == "Region IV-A", "Calabarzon", metadata$division_exposure) 
-  
-  # Change Region IV-B to Mimaropa
-  metadata$division <- ifelse(metadata$division == "Region IV-B", "Mimaropa", metadata$division) 
-  metadata$division_exposure <- ifelse(metadata$division_exposure == "Region IV-B", "Mimaropa", metadata$division_exposure) 
-  
-  # Change Region IX to Zamboanga Peninsula
-  metadata$division <- ifelse(metadata$division == "Region IX", "Zamboanga Peninsula", metadata$division) 
-  metadata$division_exposure <- ifelse(metadata$division_exposure == "Region IX", "Zamboanga Peninsula", metadata$division_exposure) 
-  
-  # Change Region V to Bicol
-  metadata$division <- ifelse(metadata$division == "Region V", "Bicol", metadata$division) 
-  metadata$division_exposure <- ifelse(metadata$division_exposure == "Region V", "Bicol", metadata$division_exposure) 
-  
-  # Change Region VI to Western Visayas
-  metadata$division <- ifelse(metadata$division == "Region VI", "Western Visayas", metadata$division) 
-  metadata$division_exposure <- ifelse(metadata$division_exposure == "Region VI", "Western Visayas", metadata$division_exposure) 
-  
-  # Change Region VIII (Eastern Visayas) to Eastern Visayas
-  metadata$division <- ifelse(metadata$division == "Region VIII (Eastern Visayas)", "Eastern Visayas", metadata$division) 
-  metadata$division_exposure <- ifelse(metadata$division_exposure == "Region VIII (Eastern Visayas)", "Eastern Visayas", metadata$division_exposure) 
-  
-  # Change Region X (Northern Mindanao) to Northern Mindanao
-  metadata$division <- ifelse(metadata$division == "Region X (Northern Mindanao)", "Northern Mindanao", metadata$division) 
-  metadata$division_exposure <- ifelse(metadata$division_exposure == "Region X (Northern Mindanao)", "Northern Mindanao", metadata$division_exposure) 
-  
-  # Change Region XI (Davao Region) to Davao Region
-  metadata$division <- ifelse(metadata$division == "Region XI (Davao Region)", "Davao Region", metadata$division) 
-  metadata$division_exposure <- ifelse(metadata$division_exposure == "Region XI (Davao Region)", "Davao Region", metadata$division_exposure) 
-  
-  # Change Region XII (Soccsksargen) to Soccsksargen
-  metadata$division <- ifelse(metadata$division == "Region XII (Soccsksargen)", "Soccsksargen", metadata$division) 
-  metadata$division_exposure <- ifelse(metadata$division_exposure == "Region XII (Soccsksargen)", "Soccsksargen", metadata$division_exposure) 
-  
-  # Change Taguig City to National Capital Region
-  metadata$division <- ifelse(metadata$division == "Taguig City", "National Capital Region", metadata$division) 
-  metadata$division_exposure <- ifelse(metadata$division_exposure == "Taguig City", "National Capital Region", metadata$division_exposure) 
-  
-  # Change Zamboanga to Zamboanga Peninsula
-  metadata$division <- ifelse(metadata$division == "Zamboanga", "Zamboanga Peninsula", metadata$division) 
-  metadata$division_exposure <- ifelse(metadata$division_exposure == "Zamboanga", "Zamboanga Peninsula", metadata$division_exposure) 
+  metadata <- rename(c("Region IX",
+                       "Zamboanga"), 
+                       "Zamboanga Peninsula")
 
+  metadata <- rename(c("Region V"), 
+                       "Bicol")
+                       
+  metadata <- rename(c("Region VI"), 
+                       "Western Visayas")
+
+  metadata <- rename(c("Region VIII (Eastern Visayas)"), 
+                       "Eastern Visayas")
+
+  metadata <- rename(c("Region X (Northern Mindanao)"), 
+                       "Northern Mindanao")
+  
+  metadata <- rename(c("Region XII (Soccsksargen)"), 
+                       "Soccsksargen")
+  
   # Drop quotes for columns: originating_lab, submitting_lab, and	authors
   metadata$originating_lab <-  gsub('"', '', metadata$originating_lab) 
   metadata$submitting_lab <-  gsub('"', '', metadata$submitting_lab) 
@@ -114,7 +82,7 @@ for (tar in input) {
   # Read metadata file
   tsvFile <- read.delim(tsvFileName, sep="\t")
   # Apply rename function
-  tsvFile <- rename(tsvFile)
+  tsvFile <- fixName(tsvFile)
   # Delete original metadata file
   file.remove(list.files(pattern=".*tsv$"))
   # Save new metadata file
--- 04-qualityCheckSequences/scripts/filter.R
+++ 04-qualityCheckSequences/scripts/filter.R
@@ -2,17 +2,16 @@
 
 ### Load QC result
 qc.result <- read.delim(file = paste0("output/", list.files(path = "output/", pattern="^[^~].*tsv$")), sep = '\t', header = TRUE)
-status.col <- as.vector(c("seqName","qc.missingData.status","qc.mixedSites.status","qc.privateMutations.status","qc.snpClusters.status","qc.frameShifts.status","qc.stopCodons.status"))
+status.col <- as.vector(c("seqName", "qc.missingData.status", "qc.mixedSites.status", "qc.privateMutations.status", "qc.snpClusters.status", "qc.frameShifts.status", "qc.stopCodons.status"))
 qc.result <- qc.result[,status.col]
 
 ### 1 - Fail, 0 - Pass
 qc.result$qc.status <- ifelse(grepl("mediocre|bad", qc.result$qc.missingData.status) |
-                                grepl("mediocre|bad", qc.result$qc.mixedSites.status) |
-                                grepl("mediocre|bad", qc.result$qc.privateMutations.status) |
-                                grepl("mediocre|bad", qc.result$qc.snpClusters.status) |
-                                grepl("mediocre|bad", qc.result$qc.frameShifts.status) |
-                                grepl("mediocre|bad", qc.result$qc.stopCodons.status)
-                                ,1 ,0)
+                              grepl("mediocre|bad", qc.result$qc.mixedSites.status) |
+                              grepl("mediocre|bad", qc.result$qc.privateMutations.status) |
+                              grepl("mediocre|bad", qc.result$qc.snpClusters.status) |
+                              grepl("mediocre|bad", qc.result$qc.frameShifts.status) |
+                              grepl("mediocre|bad", qc.result$qc.stopCodons.status), 1, 0)
 
 ### Remove unclean rows (qc.status = 1) and retain only first column
 qc.result.output <- qc.result[qc.result$qc.status != 1, ]
